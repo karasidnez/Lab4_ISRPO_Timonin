f96050819ff4ccd190aac1045d7210556cdcd566
karasidnez <karasdnez@gmail.com>
2026-02-01 17:25:09 +0300

    наполнение и создание program.cs

--- server/program.cs
+++ server/program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Привет");
+
+while (true) {
+    Console.WriteLine("Что ты хочешь узнать 1 - Фио,2 - Группу,3 - Дату ,4 - выход");
+    string? a = Console.ReadLine();
+    if (a == "1")
+    {
+        Console.WriteLine("Тимонин Иван");
+    } else if (a == "2")
+    {
+        Console.WriteLine("ИСП-233");
+    } else if(a == "3")
+    {
+        Console.WriteLine(DateTime.Now);
+    }
+    else
+    {
+        break;
+    }
+    
+}
