65683aa995a01006df42659962cb5831b7197dff
karasidnez <karasdnez@gmail.com>
2026-02-02 08:16:32 +0300

    оформление README.md

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c549e65c4d79a8794cf352cf55925da11daff8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94d6a69ad76dc4297b34deba3ce040f20de9f92")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
